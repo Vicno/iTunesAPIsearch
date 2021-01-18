333ee971bc67e1768156da6ebf5273caae58aea0
Vicno <diegorosazza@gmail.com>
2021-01-18 15:39:29 -0400

    attempting to read json from iTunes api, having cors permitions error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ItunesAPIsearch';
+  url = 'https://itunes.apple.com/search?term=jack+johnson&limit=25';
+  constructor(private http: HttpClient){
+    
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data);
+    });
+  }
 }
