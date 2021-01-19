60e56a5581b8c63e9a403a0aae9247e57af6a37b
Vicno <diegorosazza@gmail.com>
2021-01-19 19:05:02 -0400

    Grid presentation with Material V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 // Include the JSONP module for JSONP support
 import {HttpClientModule, HttpClientJsonpModule} from '@angular/common/http'
@@ -23,6 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientJsonpModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
